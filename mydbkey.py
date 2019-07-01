164b6b76ab518d342996af5237f83d35eb47eea2
Boanerges1996 <43884482+Boanerges1996@users.noreply.github.com>
2019-07-01 09:56:57 +0000

    new db hosting using remoteMysql

--- mydbkey.py
+++ mydbkey.py
@@ -10,10 +10,10 @@ class myDb():
         #     self.MYSQL_PASSWORD = 'NqpuhFBYee'
         #     self.MYSQL_DB = 'H0gs53LE43'
         if os.environ.get('second_db_ENV')=="second production":
-            self.MYQL_HOST = 'sql10.freemysqlhosting.net'
-            self.MYSQL_USER = 'sql10294510'
-            self.MYSQL_PASSWORD = 'b2mfXxfqfl'
-            self.MYSQL_DB = 'sql10294510'
+            self.MYQL_HOST = 'remotemysql.com'
+            self.MYSQL_USER = 'S1ivb2E192'
+            self.MYSQL_PASSWORD = '5Z88I0m74d'
+            self.MYSQL_DB = 'S1ivb2E192'
         else:
             self.MYQL_HOST = 'localhost'
             self.MYSQL_USER = 'root'
